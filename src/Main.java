28809e362bfbeb37435150efefb11e6483199ff7
FirmestTexas79 <matasovsky.samuel@seznam.cz>
2023-06-08 13:09:42 +0200

    Zadání

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Welcome hovnovole!");
+        System.out.printf("Welcome stranger0!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
@@ -15,3 +15,14 @@ public class Main {
         }
     }
 }
+
+
+//Níže jsou k dispozici dva soubory s daty, které si uložíte do kořenové složky projektu. Soubor people.txt obsahuje data o zákaznících a soubor tshirts.txt data o jimi objednaných třičkách. V prvním řádku souboru jsou vždy uvedeny názvy sloupců s daty.
+//
+//Vytvořte aplikaci, která propojí odpovídající záznamy podle hodnoty people.ID == tshirts.CUSTOMER_ID a zobrazí je v tabulce na obrazovce. Sloupce s ID v názvu nezobrazujte, naopak přidejte pro každý záznam/řádek sloupec s celkovou cenou pro zákazníka (tj. počet ks * cena).
+//
+//Pro zjednodušení můžete předpokládat, že každý zákazník má právě jednu objednávku triček. Pozor, pořadí řádků v obou souborech si nemusí odpovídat. Zvolte vhodnou objektovou reprezentaci.
+//
+//Soubory jsou zde:
+//people.txt
+//tshirts.txt
